73db33b15472727cd6beed43637cd5da6daa6929
Santosh19-99 <santosh08alison@gmail.com>
2025-01-17 09:13:55 +0545

    Kingsway updated

---
+++ padam.js
@@ -0,0 +1,41 @@
+// //closers vaneko parent ko property access garxa vane that is called closer, yesma lexical closer pani hunx//
+
+// // function addItem(a){
+// //     return function(){
+// //         const b = "hello world";
+// //         console.log("Hello")
+// //     }
+// // }
+
+// // const item = addItem(4);
+// // console.log("item"())
+
+
+// /callback/
+
+// function data1(a, cb){
+//     console.log(a);
+//     if cb();
+
+// }
+// data1(3, data1(x))
+
+
+// const promise = new Promise((resolve,reject))
+
+//encapsulation//
+
+
+// class Car{
+//     constructor(brand, color){
+//         this.brand = brand;
+//         this.color = color;
+//     }
+//     drive(){
+//         return`${this.color} ${this.brand} is driving`;
+//     }
+// }
+// const car1 = new Car("Toyota", "Red");
+//     console.log(car1.drive());
+
+
